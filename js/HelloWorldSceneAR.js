c537f199c02cf5af1497b260a4ad0d028a4e5f5e
vasilevladilie <vasilevladilie@gmail.com>
2021-03-27 11:51:21 +0200

    [IARA] Add a 3d object into AR scene capable of move interaction

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,55 +1,55 @@
 'use strict';
 
 import React, { Component } from 'react';
-
 import {StyleSheet} from 'react-native';
-
 import {
   ViroARScene,
   ViroText,
-  ViroConstants,
+  ViroMaterials,
+  ViroBox,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroSpotLight,
+  ViroARPlaneSelector,
+  ViroNode,
+  ViroAnimations,
 } from 'react-viro';
-
-export default class HelloWorldSceneAR extends Component {
-
-  constructor() {
-    super();
-
-    // Set initial state here
-    this.state = {
+var createReactClass = require('create-react-class');
+var HelloWorldSceneAR = createReactClass({
+  getInitialState() {
+    return {
       text : "Initializing AR..."
     };
+  },
 
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
-
-  render() {
+  render: function() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingInitialized={()=>{this.setState({text : "Hello World!"})}}>
+        <ViroText text={this.state.text} scale={[.1, .1, .1]} height={1} width={4} position={[0, .5, -1]} style={styles.helloWorldTextStyle} />
+
+        <ViroAmbientLight color={"#aaaaaa"} />
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+
+          <Viro3DObject
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            position={[0, 0, -1]}
+            scale={[.2, .2, .2]}
+            type="VRX"
+            dragType="FixedDistance" onDrag={()=>{}}
+          />
+
       </ViroARScene>
     );
-  }
-
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
-      this.setState({
-        text : "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
-}
+  },
+});
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 30,
+    fontSize: 50,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
 
